5ce792fb2339eac18dba34d778a2b18c79839490
olvmky <97950702+olvmky@users.noreply.github.com>
2024-10-21 07:44:15 -0700

    add in delete method

--- app/clients/router.py
+++ app/clients/router.py
@@ -31,7 +31,7 @@ async def predict(data: PredictionInput):
     print(data.model_dump())
     return interpret_and_calculate(data.model_dump())
 
-@router.post("/add_user")
+@router.post("/add")
 async def add(data: PredictionInput):
     # Input data to SQLite
     conn = get_db_connection()
@@ -51,8 +51,10 @@ async def add(data: PredictionInput):
             data.attending_school, data.currently_employed, data.substance_use, data.time_unemployed,
             data.need_mental_health_support_bool))
     conn.commit()
+    client_id = conn.execute("SELECT last_insert_rowid()").fetchone()[0]
     conn.close()
-    return {"prediction_result": interpret_and_calculate(data.model_dump())}
+    return { "client_id": client_id, 
+        "prediction_result": interpret_and_calculate(data.model_dump())}
 
 # get client by id
 @router.get("/{client_id}")
@@ -83,17 +85,33 @@ async def update_client(client_id: int, data: PredictionInput):
             housing = ?, income_source = ?, felony_bool = ?, attending_school = ?,
             currently_employed = ?, substance_use = ?, time_unemployed = ?, need_mental_health_support_bool = ?
         WHERE id = ?
-    ''', (
-        data.age, data.gender, data.work_experience, data.canada_workex, data.dep_num,
-        data.canada_born, data.citizen_status, data.level_of_schooling, data.fluent_english,
-        data.reading_english_scale, data.speaking_english_scale, data.writing_english_scale,
-        data.numeracy_scale, data.computer_scale, data.transportation_bool, data.caregiver_bool,
-        data.housing, data.income_source, data.felony_bool, data.attending_school,
-        data.currently_employed, data.substance_use, data.time_unemployed,
-        data.need_mental_health_support_bool, client_id
+        ''', (
+            data.age, data.gender, data.work_experience, data.canada_workex, data.dep_num,
+            data.canada_born, data.citizen_status, data.level_of_schooling, data.fluent_english,
+            data.reading_english_scale, data.speaking_english_scale, data.writing_english_scale,
+            data.numeracy_scale, data.computer_scale, data.transportation_bool, data.caregiver_bool,
+            data.housing, data.income_source, data.felony_bool, data.attending_school,
+            data.currently_employed, data.substance_use, data.time_unemployed,
+            data.need_mental_health_support_bool, client_id
     ))
     
     conn.commit()
     conn.close()
     
     return {"message": "Client information updated successfully", "client_id": client_id}
+
+# delete clients info by id
+@router.delete("/delete/{client_id}")
+async def delete_client(client_id: int):
+    conn = get_db_connection()
+    
+    client = conn.execute('SELECT * FROM clients WHERE id = ?', (client_id,)).fetchone()
+    if client is None:
+        conn.close()
+        raise HTTPException(status_code=404, detail="Client not found")
+    
+    conn.execute('DELETE FROM clients WHERE id = ?', (client_id,))
+    conn.commit()
+    conn.close()
+
+    return {"message": f"Client with id {client_id} deleted successfully."}
