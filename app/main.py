b069f4488db1eb7cf79125b1ffaed98b10a73765
qh2244 <qifenghuang@outlook.com>
2024-10-20 23:42:36 -0700

    Updated main comment

--- app/main.py
+++ app/main.py
@@ -1,23 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.clients.router import router as clients_router
-from app.clients.database import create_clients_table  # 从 database.py 导入
+from app.clients.database import create_clients_table  # input from database.py
 
-# FastAPI 实例
 app = FastAPI()
 
-# 设置 API 路由
+# Set API endpoints on router
 app.include_router(clients_router)
 
-# 配置 CORS 中间件
+# Configure CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # 允许所有源
-    allow_methods=["*"],  # 允许所有方法（包括 OPTIONS）
-    allow_headers=["*"],  # 允许所有头信息
+    allow_origins=["*"],  # Allows all origins
+    allow_methods=["*"],  # Allows all methods, including OPTIONS
+    allow_headers=["*"],  # Allows all headers
 )
 
-# 在启动时创建客户端表
 @app.on_event("startup")
 async def startup_event():
     create_clients_table()
